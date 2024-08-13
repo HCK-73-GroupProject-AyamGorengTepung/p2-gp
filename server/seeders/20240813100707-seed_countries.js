8418004f2794aafc3d63a2a63e4929a306b15641
teuku rabin <rabinrizkii@gmail.com>
2024-08-13 17:37:29 +0700

    seeding

---
+++ server/seeders/20240813100707-seed_countries.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   let data = require('../data/countries.json').map(el => {
+    el.createdAt = el.updatedAt = new Date()
+    return el
+   })
+   await queryInterface.bulkInsert('Countries', data)
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Countries', null, {truncate: true, cascade: true, restartIdentity: true})
+  }
+};
